c514ed23095e43aba62ece48c10c2b471291d6f8
dwxh-mi <1121057486@qq.com>
2021-07-14 18:33:01 +0800

    feat: [:sparkles:] 增加nginx日志刷新脚本

---
+++ linux_scripts/install_arch/files/nginx_renew.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+getdatestring()
+{
+    TZ='Asia/Chongqing' date "+%Y%m%d"
+}
+datestring=$(getdatestring)
+
+mv /var/log/nginx/access.log /var/log/nginx/access.${datestring}.log
+mv /var/log/nginx/error.log /var/log/nginx/error.${datestring}.log
+kill -USR1 `cat /var/run/nginx.pid`
